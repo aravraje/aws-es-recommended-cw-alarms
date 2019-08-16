7e5d518dd06d6db4504b458529cbedbbbc5c4e4c
araviraj <araviraj@amazon.com>
2019-08-15 20:01:40 -0400

    Added RedCluster,YellowCluster,FreeStorageSpace alarms

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
 from aws_es_recommended_cw_alarms.aws_es_recommended_cw_alarms_stack import AwsEsRecommendedCwAlarmsStack
-
+import os
 
 app = core.App()
-AwsEsRecommendedCwAlarmsStack(app, "aws-es-recommended-cw-alarms")
+
+AwsEsRecommendedCwAlarmsStack(app, "aws-es-recommended-cw-alarms", os.environ["ES_DOMAIN_ARN"].split("/")[1], env={
+    "account": os.environ["CDK_DEFAULT_ACCOUNT"] or os.environ["ES_DOMAIN_ARN"].split(":")[4],
+    "region": os.environ["CDK_DEFAULT_REGION"] or os.environ["ES_DOMAIN_ARN"].split(":")[3],
+})
 
 app.synth()
--- aws_es_recommended_cw_alarms/aws_es_recommended_cw_alarms_stack.py
+++ aws_es_recommended_cw_alarms/aws_es_recommended_cw_alarms_stack.py
@@ -1,9 +1,61 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core, 
+    aws_cloudwatch as cloudwatch, 
+    aws_lambda as _lambda
+)
 
 class AwsEsRecommendedCwAlarmsStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(
+        self, scope: core.Construct, id: str, domain_name: str, **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Setting a Cloudwatch Alarm on the ClusterStatus.red metric
+        self._RedClusterAlarm = cloudwatch.Alarm(
+            self,
+            domain_name + "-RedClusterAlarm",
+            metric=cloudwatch.Metric(
+                metric_name="ClusterStatus.red",
+                namespace="AWS/ES",
+                dimensions={"DomainName": domain_name},
+            ),
+            threshold=1,
+            comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            period=core.Duration.minutes(1),
+            evaluation_periods=1,
+            statistic="max",
+            treat_missing_data=cloudwatch.TreatMissingData.NOT_BREACHING,
+        )
+
+        # Setting a Cloudwatch Alarm on the ClusterStatus.yellow metric
+        self._YellowClusterAlarm = cloudwatch.Alarm(
+            self,
+            domain_name + "-YellowClusterAlarm",
+            metric=cloudwatch.Metric(
+                metric_name="ClusterStatus.yellow",
+                namespace="AWS/ES",
+                dimensions={"DomainName": domain_name},
+            ),
+            threshold=1,
+            comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            period=core.Duration.minutes(1),
+            evaluation_periods=1,
+            statistic="max",
+            treat_missing_data=cloudwatch.TreatMissingData.NOT_BREACHING,
+        )
+
+        self._FreeStorageSpaceAlarm = cloudwatch.Alarm(
+            self,
+            domain_name + "-FreeStorageSpaceAlarm",
+            metric=cloudwatch.Metric(
+                metric_name="FreeStorageSpace",
+                namespace="AWS/ES",
+                dimensions={"DomainName": domain_name},
+            ),
+            threshold=20480,
+            comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
+            period=core.Duration.minutes(1),
+            evaluation_periods=1,
+            statistic="min",
+            treat_missing_data=cloudwatch.TreatMissingData.IGNORE,
+        )
