fd56e2f731ba7733725cad74635f0c2961d0d1a2
araviraj <15716966+aravraje@users.noreply.github.com>
2019-11-10 18:49:50 -0500

    Added exponential backoff retry strategy while calling ES APIs in the Lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -37,39 +37,41 @@ METRIC_TO_API_MAPPING = {
 
 
 def lambda_handler(event, context):
-    if not "IS_CW_ALARM" in event["Records"][0]["Sns"]["MessageAttributes"]:
-        sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
-        cw_metric = sns_message["Trigger"]["MetricName"]
+    if "IS_CW_ALARM" in event["Records"][0]["Sns"]["MessageAttributes"]:
+        return
+    
+    sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
+    cw_metric = sns_message["Trigger"]["MetricName"]
 
-        host = "https://" + os.environ["DOMAIN_ENDPOINT"] + "/"
-        region = os.environ["DOMAIN_ARN"].split(":")[3]
-        service = os.environ["DOMAIN_ARN"].split(":")[2]
-        awsauth = AWS4Auth(
-            os.environ["AWS_ACCESS_KEY_ID"],
-            os.environ["AWS_SECRET_ACCESS_KEY"],
-            region,
-            service,
-            session_token=os.environ["AWS_SESSION_TOKEN"],
+    host = "https://" + os.environ["DOMAIN_ENDPOINT"] + "/"
+    region = os.environ["DOMAIN_ARN"].split(":")[3]
+    service = os.environ["DOMAIN_ARN"].split(":")[2]
+    awsauth = AWS4Auth(
+        os.environ["AWS_ACCESS_KEY_ID"],
+        os.environ["AWS_SECRET_ACCESS_KEY"],
+        region,
+        service,
+        session_token=os.environ["AWS_SESSION_TOKEN"],
+    )
+    headers = {"Content-Type": "application/json"}
+
+    if "SNS_TOPIC_ARN" in os.environ:
+        send_to_es(
+            host,
+            cw_metric,
+            METRIC_TO_API_MAPPING[cw_metric],
+            awsauth,
+            headers,
+            os.environ["SNS_TOPIC_ARN"]
         )
-        headers = {"Content-Type": "application/json"}
-
-        if "SNS_TOPIC_ARN" in os.environ:
-            send_to_es(
-                host,
-                cw_metric,
-                METRIC_TO_API_MAPPING[cw_metric],
-                awsauth,
-                headers,
-                os.environ["SNS_TOPIC_ARN"]
-            )
-        else:
-            send_to_es(
-                host, 
-                cw_metric, 
-                METRIC_TO_API_MAPPING[cw_metric], 
-                awsauth, 
-                headers
-            )
+    else:
+        send_to_es(
+            host, 
+            cw_metric, 
+            METRIC_TO_API_MAPPING[cw_metric], 
+            awsauth, 
+            headers
+        )
 
 
 def send_to_es(host, cw_metric, apis, awsauth, headers, sns_topic_arn=None):
@@ -78,32 +80,36 @@ def send_to_es(host, cw_metric, apis, awsauth, headers, sns_topic_arn=None):
     }
 
     for api in apis:
+        num_retries, max_retries = 0, 3
+        es_backoff = 5 # In Seconds
         out = {}
         out["domain_endpoint"] = os.environ["DOMAIN_ENDPOINT"]
         out["metric_name"] = cw_metric
         out["api"] = api
-        try:
-            r = requests.get(host + api, auth=awsauth, headers=headers, timeout=30)
-            r.raise_for_status()
-            out["response"] = r.text
+        while num_retries < max_retries:
+            try:
+                r = requests.get(host + api, auth=awsauth, headers=headers, timeout=30)
+                r.raise_for_status()
+                out["response"] = r.text
+                out.pop("exception", None)
+                break
 
-        except Exception as e:
-            # print(f"An unknown error occurred while invoking the API - {api}: {e}")
-            out["exception"] = e
-
-        finally:
-            print(out)
+            except Exception as e:
+                out["exception"] = e
+                num_retries += 1
+                sleep(num_retries * es_backoff)
 
         api_output["apis"].append(out)
 
-    if api_output["apis"] and sns_topic_arn:
+    print(f"ES API output: \n{api_output}")
+
+    if sns_topic_arn:
         sns = boto3.client("sns")
-        max_retries = 3
-        num_retries = 0
-        backoff = 1  # In Seconds
+        num_retries, max_retries = 0, 3
+        sns_backoff = 1  # In Seconds
         sns_response = None
 
-        while num_retries <= max_retries:
+        while num_retries < max_retries:
             sns_response = sns.publish(
                 TopicArn=sns_topic_arn,
                 Message=api_output,
@@ -116,9 +122,7 @@ def send_to_es(host, cw_metric, apis, awsauth, headers, sns_topic_arn=None):
                 break
 
             num_retries += 1
-            sleep(num_retries * backoff)
+            sleep(num_retries * sns_backoff)
 
-        if num_retries > max_retries:
-            print(
-                f"An unknown error occurred while publishing to the SNS topic: {sns_response}"
-            )
+        if num_retries == max_retries:
+            print(f"An unknown error occurred while publishing the ES API output to the SNS topic: \n{sns_response}")
